905ed43855196bd1a535542f6ade1d06fd12725d
danicabrera <69865919+danicabrera@users.noreply.github.com>
2022-10-18 00:18:24 -0600

    Instructor implementado

---
+++ Instructor.py
@@ -0,0 +1,36 @@
+class Instructor:
+
+    def __init__(self, id, nombre, contrato, estadoCuenta, pago):
+        self.ID = id
+        self.nombre = nombre
+        self.contrato = contrato
+        self.estadoCuenta = estadoCuenta
+        self.metodoPago = pago
+
+    def getID(self):
+        return self.ID
+
+    def getNombre(self):
+        return self.nombre
+
+    def getContrato(self):
+        return self.contrato
+
+    def setContrato(self, nuevoContrato):
+        self.contrato = nuevoContrato
+        return None
+
+    def getEstadoCuenta(self):
+        return self.estadoCuenta
+
+    def setEstadoCuenta(self, nuevoEstado):
+        self.estadoCuenta = nuevoEstado
+        return None
+
+    def getMetodoPago(self):
+        return self.metodoPago
+
+    def cambioMetodoPago(self, nuevoMetodo):
+        self.metodoPago = nuevoMetodo
+        return None
+
