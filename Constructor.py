339a5e7827d71fbd1c85d724d2b1304741da80e8
danicabrera <69865919+danicabrera@users.noreply.github.com>
2022-11-22 11:25:12 -0600

    Divsion de admins

--- Constructor.py
+++ Constructor.py
@@ -13,7 +13,7 @@ class Constructor:
     def __init__(self):
         self.entrenador_zero = Instructor(0, "Nombre", "contrato", "estadoCuenta", "metodoPago")
         self.sesion_zero = Sesion("", "", "", "", "", 0)
-        self.usuario_zero = Persona("", "", "", "", "", "", "", contrasena="1", pago="", id="1")
+        self.usuario_zero = Persona("", "", "", "", "", "", plan= "ADMIN 3", contrasena="1", pago="", id="1")
         self.usuario_uno = Persona("", "", "", "", "", "", "", contrasena="123", pago="", id="123")
         self.usuario_zero.setAdminStatus(True)
         self.sesiones = [self.sesion_zero]
@@ -24,6 +24,8 @@ class Constructor:
         # Jalas
 
         self.instructores = self.datos.jalar_instructores(self.instructores)
+        self.usuarios = self.datos.jalar_usuarios(self.usuarios)
+        self.sesiones = self.datos.jalar_sesiones(self.sesiones)
 
     def menuInicial(self):
         valor_salida = False
@@ -118,27 +120,20 @@ class Constructor:
 
         return None
 
-    def menuAdmin(self, persona):
+    #Menú de usuarios
+    def menuAdmin1(self, persona):
         valor_salida = False
         while not valor_salida:
             print("Bienvenido al menu ADMIN de IHealth")
             print("Por favor ingrese el numero de la opción que desea tomar:")
-            print("0. Crear Usuario.")
-            print("1. Agregar Instructor.")
-            print("2. Modificar Instructor.")
-            print("3. Dar de baja instructor.")
-            print("4. Agregar sesion.")
-            print("5. Modificar sesion.")
-            print("6. Dar de baja sesion.")
-            print("7. Modificar usuarios.")
-            print("8. Dar de baja usuario.")
-            print("9. Modulo de Reportes.")
-            print("10. Simulación de sesión.")
-            print("11. Salir.")
+            print("1. Crear Usuario.")
+            print("2. Modificar usuarios.")
+            print("3. Dar de baja usuario.")
+            print("4. Salir.")
 
             x = int(input(""))
 
-            if x == 0:
+            if x == 1:
                 # Crear Usuario
                 nombre = input("Ingrese el nombre del usuario:  ")
                 edad = input("Ingrese la edad del usuario:  ")
@@ -158,7 +153,191 @@ class Constructor:
                 self.datos.agregarUsuario(usuario)
                 self.imprimirUsuarios()
 
-            elif x == 1:
+            elif x == 2:
+                # Modificar Usuario
+                usuario = self.usuario_zero
+                idUser = input("Por favor ingrese el ID del Usuario que desea modificar:")
+                valor = 0
+                for l in range(len(self.usuarios)):
+                    if self.usuarios[l].getID() == idUser:
+                        usuario = self.usuarios[l]
+                        self.usuarios.pop(l)
+                        valor = l
+
+                print("Por favor ingrese el numero del aspecto que desea modificar:")
+                print("1. Nombre.")
+                print("2. Edad.")
+                print("3. Altura.")
+                print("4. Calorie Intake.")
+                print("5. Contrasena.")
+                print("6. Peso Actual.")
+                print("7. Plan.")
+                print("8. Pago.")
+                print("9. Acceso.")
+                print("10. Admin Status.")
+
+                eleccion = int(input(""))
+                nuevoDato = input("Ingrese el nuevo dato a reemplazarlo: ")
+                if eleccion == 1:
+                    usuario.setNombre(nuevoDato)
+                elif eleccion == 2:
+                    usuario.setEdad(nuevoDato)
+                elif eleccion == 3:
+                    usuario.setAltura(nuevoDato)
+                elif eleccion == 4:
+                    usuario.setCalorieIntake(nuevoDato)
+                elif eleccion == 5:
+                    usuario.setContrasena(nuevoDato)
+                elif eleccion == 6:
+                    usuario.setPesoActual(nuevoDato)
+                elif eleccion == 7:
+                    usuario.setPlan(nuevoDato)
+                elif eleccion == 8:
+                    usuario.setMetodoPago(nuevoDato)
+                elif eleccion == 9:
+                    usuario.setAcceso(nuevoDato)
+                elif eleccion == 10:
+                    usuario.setAdminStatus(nuevoDato)
+
+                self.usuarios.insert(valor, usuario)
+                self.datos.agregarUsuario(usuario)
+                usuario.imprimir()
+
+            elif x == 3:
+                # Dar de baja usuario
+                usuario = self.usuario_zero
+                idUser = input("Por favor ingrese el ID del Usuario que desea dar de baja:")
+                for l in range(len(self.usuarios)):
+                    if self.usuarios[l].getID() == idUser:
+                        usuario = self.usuarios[l]
+                        usuario.finalizarSuscripcion()
+                        usuario.imprimir()
+
+            elif x == 4:
+                valor_salida = True
+            else:
+                print("Error. Intente de nuevo.")
+
+        return None
+
+    #Menu sesiones
+    def menuAdmin2(self, persona):
+        valor_salida = False
+        while not valor_salida:
+            print("Bienvenido al menu ADMIN de IHealth")
+            print("Por favor ingrese el numero de la opción que desea tomar:")
+            print("1. Agregar sesion.")
+            print("2. Modificar sesion.")
+            print("3. Dar de baja sesion.")
+            print("4. Simulación de sesión.")
+            print("5. Salir.")
+
+            x = int(input(""))
+
+
+            if x == 1:
+                # Agregar Sesion
+                fecha = input("Ingrese la fecha de la sesión:  ")
+                hora = input("Ingrese la hora de la sesión:  ")
+                duracion = input("Ingrese la duración de la sesión:  ")
+                instructor = input("Ingrese el ID del Instructor:  ")
+                categoria = input("Ingrese la categoria de la sesión:  ")
+                ultimaSesion = self.sesiones[len(self.sesiones) - 1]
+                nuevoID = str(int(ultimaSesion.id_sesion) + 1)
+
+                sesion = Sesion(fecha, hora, duracion, instructor, categoria, nuevoID)
+
+                self.sesiones.append(sesion)
+                self.imprimirSesiones()
+                self.datos.agregarSesion(sesion)
+                sesion.imprimir()
+
+            elif x == 2:
+                # Modificar Sesion
+                sesion = self.sesion_zero
+                idSesion = input("Por favor ingrese el ID de la Sesion que desea modificar:")
+                valor = 0
+                for l in range(len(self.sesiones)):
+                    if self.sesiones[l].getId_sesion() == idSesion:
+                        sesion = self.sesiones[l]
+                        self.sesiones.pop(l)
+                        valor = l
+
+                print("Por favor ingrese el numero del aspecto que desea modificar:")
+                print("1. Fecha.")
+                print("2. Hora.")
+                print("3. Duracion.")
+                print("4. Instructor.")
+                print("5. Categoria.")
+
+                eleccion = int(input(""))
+                nuevoDato = input("Ingrese el nuevo dato a reemplazarlo: ")
+                if eleccion == 1:
+                    sesion.setFecha(nuevoDato)
+                elif eleccion == 2:
+                    sesion.setHora(nuevoDato)
+                elif eleccion == 3:
+                    sesion.setDuracion(nuevoDato)
+                elif eleccion == 4:
+                    sesion.setInstructor(nuevoDato)
+                elif eleccion == 5:
+                    sesion.setCategoria(nuevoDato)
+
+                self.sesiones.insert(valor, sesion)
+                self.datos.agregarSesion(sesion)
+                sesion.imprimir()
+
+            elif x == 3:
+                # Dar de baja sesion
+                sesion = self.sesion_zero
+                idSesion = input("Por favor ingrese el ID de la Sesion que desea modificar:")
+                for l in range(len(self.sesiones)):
+                    if self.sesiones[l].getId_sesion() == idSesion:
+                        sesion = self.sesiones[l]
+                        self.sesiones.pop(l)
+
+            elif x == 4:
+                fechaSesion = input("Por favor ingrese la fecha de la sesion:")
+                horaSesion = input("Por favor ingrese la hora de la sesion:")
+                numUsuarios = int(input("Por favor ingrese el número de usuarios a ingresar:"))
+                ultimaSesion = self.sesiones[len(self.sesiones) - 1]
+                nuevoID = ultimaSesion.id_sesion + 1
+
+                rc = random.randint(50, 250)
+                calorias = random.randint(10, 1250)
+
+                decision = random.randint(0, 5)
+                ejercicio = ""
+                if decision == 0:
+                    ejercicio = "Aerobicos"
+
+                for i in range(numUsuarios):
+                    sesion = Sesion_watch(nuevoID, i, rc, ejercicio, calorias, horaSesion, fechaSesion)
+                    self.datos.agregariWatch(sesion)
+
+            elif x == 5:
+                valor_salida = True
+            else:
+                print("Error. Intente de nuevo.")
+
+        return None
+
+    #Admin instructor y reportes
+    def menuAdmin3(self, persona):
+        valor_salida = False
+        while not valor_salida:
+            print("Bienvenido al menu ADMIN de IHealth")
+            print("Por favor ingrese el numero de la opción que desea tomar:")
+            print("1. Agregar Instructor.")
+            print("2. Modificar Instructor.")
+            print("3. Dar de baja instructor.")
+            print("4. Modulo de Reportes.")
+            print("5. Salir.")
+
+            x = int(input(""))
+
+
+            if x == 1:
                 # Agregar Instructor
                 nombre = input("Ingrese el nombre del instructor:  ")
                 contrato = input("Ingrese el contrato del instructor:  ")
@@ -215,131 +394,8 @@ class Constructor:
                         instructor = self.instructores[l]
                         self.instructores.pop(l)
 
+
             elif x == 4:
-                # Agregar Sesion
-                fecha = input("Ingrese la fecha de la sesión:  ")
-                hora = input("Ingrese la hora de la sesión:  ")
-                duracion = input("Ingrese la duración de la sesión:  ")
-                instructor = input("Ingrese el ID del Instructor:  ")
-                categoria = input("Ingrese la categoria de la sesión:  ")
-                ultimaSesion = self.sesiones[len(self.sesiones) - 1]
-                nuevoID = str(int(ultimaSesion.id_sesion) + 1)
-
-                sesion = Sesion(fecha, hora, duracion, instructor, categoria, nuevoID)
-
-                self.sesiones.append(sesion)
-                self.imprimirSesiones()
-                self.datos.agregarSesion(sesion)
-                sesion.imprimir()
-
-            elif x == 5:
-                # Modificar Sesion
-                sesion = self.sesion_zero
-                idSesion = input("Por favor ingrese el ID de la Sesion que desea modificar:")
-                valor = 0
-                for l in range(len(self.sesiones)):
-                    if self.sesiones[l].getId_sesion() == idSesion:
-                        sesion = self.sesiones[l]
-                        self.sesiones.pop(l)
-                        valor = l
-
-                print("Por favor ingrese el numero del aspecto que desea modificar:")
-                print("1. Fecha.")
-                print("2. Hora.")
-                print("3. Duracion.")
-                print("4. Instructor.")
-                print("5. Categoria.")
-
-                eleccion = int(input(""))
-                nuevoDato = input("Ingrese el nuevo dato a reemplazarlo: ")
-                if eleccion == 1:
-                    sesion.setFecha(nuevoDato)
-                elif eleccion == 2:
-                    sesion.setHora(nuevoDato)
-                elif eleccion == 3:
-                    sesion.setDuracion(nuevoDato)
-                elif eleccion == 4:
-                    sesion.setInstructor(nuevoDato)
-                elif eleccion == 5:
-                    sesion.setCategoria(nuevoDato)
-
-                self.sesiones.insert(valor, sesion)
-                self.datos.agregarSesion(sesion)
-                sesion.imprimir()
-
-            elif x == 6:
-                # Dar de baja sesion
-                sesion = self.sesion_zero
-                idSesion = input("Por favor ingrese el ID de la Sesion que desea modificar:")
-                for l in range(len(self.sesiones)):
-                    if self.sesiones[l].getId_sesion() == idSesion:
-                        sesion = self.sesiones[l]
-                        self.sesiones.pop(l)
-
-
-
-            elif x == 7:
-                # Modificar Usuario
-                usuario = self.usuario_zero
-                idUser = input("Por favor ingrese el ID del Usuario que desea modificar:")
-                valor = 0
-                for l in range(len(self.usuarios)):
-                    if self.usuarios[l].getID() == idUser:
-                        usuario = self.usuarios[l]
-                        self.usuarios.pop(l)
-                        valor = l
-
-                print("Por favor ingrese el numero del aspecto que desea modificar:")
-                print("1. Nombre.")
-                print("2. Edad.")
-                print("3. Altura.")
-                print("4. Calorie Intake.")
-                print("5. Contrasena.")
-                print("6. Peso Actual.")
-                print("7. Plan.")
-                print("8. Pago.")
-                print("9. Acceso.")
-                print("10. Admin Status.")
-
-                eleccion = int(input(""))
-                nuevoDato = input("Ingrese el nuevo dato a reemplazarlo: ")
-                if eleccion == 1:
-                    usuario.setNombre(nuevoDato)
-                elif eleccion == 2:
-                    usuario.setEdad(nuevoDato)
-                elif eleccion == 3:
-                    usuario.setAltura(nuevoDato)
-                elif eleccion == 4:
-                    usuario.setCalorieIntake(nuevoDato)
-                elif eleccion == 5:
-                    usuario.setContrasena(nuevoDato)
-                elif eleccion == 6:
-                    usuario.setPesoActual(nuevoDato)
-                elif eleccion == 7:
-                    usuario.setPlan(nuevoDato)
-                elif eleccion == 8:
-                    usuario.setMetodoPago(nuevoDato)
-                elif eleccion == 9:
-                    usuario.setAcceso(nuevoDato)
-                elif eleccion == 10:
-                    usuario.setAdminStatus(nuevoDato)
-
-                self.usuarios.insert(valor, usuario)
-                self.datos.agregarUsuario(usuario)
-                usuario.imprimir()
-
-
-            elif x == 8:
-                # Dar de baja usuario
-                usuario = self.usuario_zero
-                idUser = input("Por favor ingrese el ID del Usuario que desea dar de baja:")
-                for l in range(len(self.usuarios)):
-                    if self.usuarios[l].getID() == idUser:
-                        usuario = self.usuarios[l]
-                        usuario.finalizarSuscripcion()
-                        usuario.imprimir()
-
-            elif x == 9:
                 # Modulo Estadisticas
                 self.conexion = psycopg2.connect(host='localhost', database='proyecto2', user='postgres',
                                                  password='123456', )
@@ -390,26 +446,7 @@ class Constructor:
 
                 print(horaPico)
 
-            elif x == 10:
-                fechaSesion = input("Por favor ingrese la fecha de la sesion:")
-                horaSesion = input("Por favor ingrese la hora de la sesion:")
-                numUsuarios = int(input("Por favor ingrese el ID del Usuario que desea modificar:"))
-                ultimaSesion = self.sesiones[len(self.sesiones) - 1]
-                nuevoID = ultimaSesion.id_sesion + 1
-
-                rc = random.randint(50, 250)
-                calorias = random.randint(10, 1250)
-
-                decision = random.randint(0, 5)
-                ejercicio = ""
-                if decision == 0:
-                    ejercicio = "Aerobicos"
-
-                for i in range(numUsuarios):
-                    sesion = Sesion_watch(nuevoID, i, rc, ejercicio, calorias, horaSesion, fechaSesion)
-                    self.datos.agregariWatch(sesion)
-
-            elif x == 11:
+            elif x == 5:
                 valor_salida = True
             else:
                 print("Error. Intente de nuevo.")
@@ -440,13 +477,18 @@ class Constructor:
         self.datos.agregarUsuario(usuario)
 
         if usuario.admin == True:
-            self.menuAdmin(usuario)
+            if plan == "ADMIN 1":
+                self.menuAdmin1(usuario)
+            elif plan == "ADMIN 2":
+                self.menuAdmin2(usuario)
+            elif plan == "ADMIN 3":
+                self.menuAdmin3(usuario)
         else:
             self.menuNormal(usuario)
 
     def inicioSesion(self):
         entrada = False
-        usuario = self.usuario_zero
+        usuarios = self.usuario_zero
 
         valor = 0
         while not entrada:
@@ -464,10 +506,14 @@ class Constructor:
                     print(usuario.getContrasena())
 
         if usuarios.getAdminStatus():
-
-            self.menuAdmin(usuario)
-        else:
-            self.menuNormal(usuario)
+            if usuarios.getPlan() == "ADMIN 1":
+                self.menuAdmin1(usuarios)
+            elif usuarios.getPlan() == "ADMIN 2":
+                self.menuAdmin2(usuarios)
+            elif usuarios.getPlan() == "ADMIN 3":
+                self.menuAdmin3(usuarios)
+            else:
+                self.menuNormal(usuarios)
 
     def imprimirSesiones(self):
 
